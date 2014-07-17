2a12a5c1ca7e027560c29677818f2612f7e90b79
Ruslan Shashkov <rshashkov@icloud.com>
2014-07-17 14:19:03 +0400

    Protocol definer

--- libs/protocol.js
+++ libs/protocol.js
@@ -3,8 +3,6 @@ var _ = require('lodash');
 var Protocol = function(data, bytes, options){
     var _this = this;
 
-    this.name = '';
-
     this.options = {
         onClientSayByeBye: function(){
 
@@ -17,33 +15,33 @@ var Protocol = function(data, bytes, options){
 
     _.extend(this.options, options);
 
-    var detect = function(){
+    this.detect = function(){
         if (data.substr(0, 4) == '2424') {
             console.log('Protocol: start processing Meiligao');
-            _this.name = 'meiligao';
+            return 'meiligao';
 
         //Galileo
         } else if (data.substr(0, 2) == '01') {
             console.log({message: 'Start processing Galileo Protocol'});
-            _this.name = 'galileo';
+            return 'galileo';
 
         //Iphone [$iphone]
         } else if (data.substr(0, 14) == '246970686f6e65') {
             console.log({message: 'Start processing iPhone Protocol'});
-            _this.name = 'iphone';
+            return 'iphone';
 
         //Bye
         } else if (data == '6279650d0a' || data == '657869740d0a') {
             console.log('Protocol: Client say "Good bye!"');
-            _this.options.onClientSayByeBye();
+            this.options.onClientSayByeBye();
 
         } else {
             console.log('Protocol: protocol is undefined');
-            _this.options.onProtocolUndefined();
+            this.options.onProtocolUndefined();
         }
     };
 
-    detect();
+    this.name = this.detect();
 };
 
 module.exports = Protocol;
