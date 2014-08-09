499b12b6fa67425dabcf6319916e9c9052d2ba2e
Ruslan Shashkov <rshashkov@icloud.com>
2014-08-09 13:19:51 +0400

    2

--- app.js
+++ app.js
@@ -5,25 +5,26 @@ var gz = require('./libs/geozone.js');
 var dev = require('./libs/device.js');
 var usr = require('./libs/user.js');
 
+
 /*
 usr.getById('53dfe91d47c29cea8d0006dd', function(data){
     console.log(data)
 });
+*/
 
-
-
+/*
 dev.getByIdAndIMEI('2', '868204001577450', function(data){
     console.log(data)
 });
 
 
-
+/*
 gz.checkPoint([
     55.758904,
     37.618527
 ], function(data){
     console.log(data)
 });
- */
+*/
 
 var server = new Server();
--- libs/device.js
+++ libs/device.js
@@ -6,6 +6,10 @@ var models = require('./models.js');
  */
 var Device = function () {
     this.getByIdAndIMEI = function(id, imei, done){
+		if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+			return done(false);
+		}
+		
         models.Device.findOne({id: id, imei: imei}, function(err, data){
             if(err){
                 console.log('Model Device error: model error', err);
@@ -17,6 +21,10 @@ var Device = function () {
     };
 
     this.update = function(device_id, user_id, data, done){
+		if (!device_id.match(/^[0-9a-fA-F]{24}$/)) {
+			return done(false);
+		}
+		
         models.Device.findOneAndUpdate({ _id: device_id, user: user_id }, data, function(err, data){
             if(err){
                 console.log('Model Device error: model error', err);
